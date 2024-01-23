0a4ad53a71b18bcba8ea6fa40e553261dccb168b
Shivanshu <pshivanshu99@gmail.com>
2024-01-23 17:56:37 +0530

    question bug fix

--- model/Question.js
+++ model/Question.js
@@ -4,7 +4,6 @@ const questionSchema = new mongoose.Schema({
   text: { type: String, required: true },
   options: [{ type: String, required: true }],
   correctOption: { type: String, required: true },
-  rating:{type:Number,required:true}
 });
 
 const Question = mongoose.model('Question', questionSchema);
